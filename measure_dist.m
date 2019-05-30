c437474d5b2726e8b09ff691a3bbca5a714ccadf
Simon <simonaer@stanford.edu>
2019-05-30 02:19:06 -0700

    updated measurement eqns

---
+++ measure_dist.m
@@ -0,0 +1,11 @@
+function [y,R]=measure_dist(x,m)
+a0=1;%m^2
+a1=1;%m^2
+a2=0.0024;%m^2
+
+r=norm(m-x(1:2));%true distance 
+sig_dis = a2*(r-a1)^2 + a0; %variance_dis
+R = sqrt(sig_dis);
+y=norm(m-x(1:2))+mvnrnd(0,sqrt(sig_dis));% distance measurement
+end
+
