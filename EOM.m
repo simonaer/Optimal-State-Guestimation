e02818f6332b34a7b6a30109c3b9325ac6632a6f
Simon <simonaer@stanford.edu>
2019-05-29 23:09:54 -0700

    updated scheme to reflect EKF, update frequency

---
+++ EOM.m
@@ -0,0 +1,22 @@
+function rates = EOM(x,u)
+%Equation of Motion
+%Returns the rates of states given current states and control inputs
+%rates, x, u are all column vectors
+%rates = [xdot ydot thetadot mxdot mydot]'
+%x = [x y theta mx my]'
+%u = [v thetadot]'
+
+v = u(1);
+thetadot = u(2);
+theta = x(3);
+
+rates = [v*cos(theta);
+        v*sin(theta);
+        thetadot;
+        0;
+        0];
+
+
+
+
+end
